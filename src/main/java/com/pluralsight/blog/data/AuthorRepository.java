55098a6d3a75598d3b5839b8379ae54e3f209a3d
dantaayy <dantaayy@gmail.com>
2023-01-24 09:55:38 -0700

    fixed data typo to pass pluralsight test

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestResource
 @RepositoryRestResource(exported = false)
 public interface AuthorRepository extends JpaRepository<Author, Long>{
 
